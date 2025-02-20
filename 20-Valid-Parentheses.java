c98e81f0f36409625f32b7ef12a94353a41865fc
Vaishnavi Yadav <vaishnavi.2426mca196@kiet.edu>
2025-02-20 22:44:07 +0530

    [20] [Time Beats: 97.69%] [Memory Beats: 21.30%] - LeetPush

---
+++ 20-Valid-Parentheses.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isValid(String s) {
+\tStack<Character> stack = new Stack<Character>();
+\tfor (char c : s.toCharArray()) {
+\t\tif (c == '(')
+\t\t\tstack.push(')');
+\t\telse if (c == '{')
+\t\t\tstack.push('}');
+\t\telse if (c == '[')
+\t\t\tstack.push(']');
+\t\telse if (stack.isEmpty() || stack.pop() != c)
+\t\t\treturn false;
+\t}
+\treturn stack.isEmpty();
+ }
+}
