a79bb00f795f7f8086dc33eedde897808829aae3
Vaishnavi Yadav <vaishnavi.2426mca196@kiet.edu>
2025-02-24 12:10:37 +0530

    [128] [Time Beats: 34.95%] [Memory Beats: 27.06%] - LeetPush

---
+++ 128-Longest-Consecutive-Sequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int>seqMap;
+        int longest=0;
+        for(int num : nums){
+            if(seqMap.find(num)==seqMap.end()){
+                int left=seqMap.count(num-1)?seqMap[num-1]:0;
+                int right=seqMap.count(num+1)?seqMap[num+1]:0;
+                int length=left+1+right;
+                seqMap[num]=length;
+                seqMap[num-left]=length;
+                seqMap[num+right]=length;
+                longest=max(longest,length);
+            }
+        }
+        return longest;
+    }
+};
